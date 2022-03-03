c5f9ae2d282339d63f948e9f9dabbec7ab4f96de
hanishamajety <54390105+hanishamajety@users.noreply.github.com>
2022-03-04 00:06:49 +0530

    CaseStudy1 Assignment

---
+++ casestudy1/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casestudy1
+{
+    class Course
+    {
+        public int CID { get; set; }
+        public string CName { get; set; }
+        public string Duration { get; set; }
+        public int Fees { get; set; }
+
+        public Course(int CID, string CName, string Duration, int Fees)
+        {
+            this.CID = CID;
+            this.CName = CName;
+            this.Duration = Duration;
+            this.Fees = Fees;
+        }
+    }
+}
